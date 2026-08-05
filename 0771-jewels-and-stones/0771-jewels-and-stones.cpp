a46c20fce084f1ea2def6e83b6da30ac76784480
Vikash Kumar <kumarvikash.prog@gmail.com>
2026-08-06 01:41:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.08%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> jewelSet;
+
+        for (char ch : jewels) {
+            jewelSet.insert(ch);
+        }
+
+        int count = 0;
+
+        for (char ch : stones) {
+            if (jewelSet.count(ch)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
